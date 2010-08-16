4153562e9100d5389dd8aef540ef4522ccffd5cc
Neil <ndmanvar@yahoo-inc.com>
2010-08-16 13:59:30 -0700

    no green arrow

---
+++ testi/work please/src/build/CMakeFiles/FileAccess.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FileAccess.dir/Users/ndmanvar/Desktop/fatest/bp-fileaccess/external/bp-service-tools/CppTools/src/bptypeutil.cpp.o"
+  "CMakeFiles/FileAccess.dir/Users/ndmanvar/Desktop/fatest/bp-fileaccess/external/bp-service-tools/CppTools/src/bpurlutil.cpp.o"
+  "CMakeFiles/FileAccess.dir/Users/ndmanvar/Desktop/fatest/bp-fileaccess/external/bp-service-tools/CppTools/src/bpservicedescription.cpp.o"
+  "CMakeFiles/FileAccess.dir/Users/ndmanvar/Desktop/fatest/bp-fileaccess/external/bp-service-tools/CppTools/src/bpserviceversion.cpp.o"
+  "CMakeFiles/FileAccess.dir/service.cpp.o"
+  "CMakeFiles/FileAccess.dir/littleuuid.cpp.o"
+  "CMakeFiles/FileAccess.dir/FileServer.cpp.o"
+  "CMakeFiles/FileAccess.dir/util/fileutil.cpp.o"
+  "CMakeFiles/FileAccess.dir/util/bpsync_UNIX.cpp.o"
+  "libFileAccess.pdb"
+  "libFileAccess.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FileAccess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
