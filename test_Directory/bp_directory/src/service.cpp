9ac07cbe8f4e53a3e5c2fb17cf261452b5e68e0f
Neil <ndmanvar@yahoo-inc.com>
2010-08-16 17:13:42 -0700

    bp_dir

---
+++ test_Directory/bp_directory/src/service.cpp
@@ -0,0 +1,362 @@
+/**
+ * ***** BEGIN LICENSE BLOCK *****
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * 
+ * The Original Code is BrowserPlus (tm).
+ * 
+ * The Initial Developer of the Original Code is Yahoo!.
+ * Portions created by Yahoo! are Copyright (C) 2006-2009 Yahoo!.
+ * All Rights Reserved.
+ * 
+ * Contributor(s): 
+ * ***** END LICENSE BLOCK ***** */
+
+#include "bpservice/bpservice.h"
+#include "bpservice/bpcallback.h"
+#include "bp-file/bpfile.h"
+#include <map>
+
+#if defined(WIN32)
+#include <windows.h>
+#endif
+
+using namespace std;
+using namespace bp::file;
+using namespace bplus::service;
+namespace bfs = boost::filesystem;
+
+const size_t kDefaultLimit = 1000;
+
+// A visitor class for list() and listWithStructure().  Applies
+// filtering, enforces max nodes visited, and builds up a 
+// bplus::List* of matched nodes.
+//
+class ListVisitor : public IVisitor {
+public:
+    ListVisitor(bool flat)
+        :  m_flat(flat), m_mimeTypes(), m_limit(kDefaultLimit),
+           m_cb(NULL), m_visited(0), m_topList(new bplus::List)  {
+    }
+
+    virtual ~ListVisitor() {
+        if (m_topList) {
+            delete m_topList;
+        }
+        if (m_cb) {
+            delete m_cb;
+        }
+    }
+
+    IVisitor::tResult visitNode(const Path& p,
+                                const Path& relPath) {
+        // enforce max nodes visited
+        if (m_visited++ >= m_limit) {
+            return eStop;
+        }
+
+        // apply mimetype filtering
+        if (isMimeType(p, m_mimeTypes)) {
+            if (m_cb) {
+                bplus::Map m;
+                m.add("handle", new bplus::Path(p.externalUtf8()));
+                if (!m_flat) {
+                    m.add("relativeName",
+                          new bplus::String(relPath.externalUtf8()));
+                }
+                m_cb->invoke(m);
+            }
+
+            // add this child
+            addChild(p, relPath);
+        }
+        return eOk;
+    }
+
+    void setMimeTypes(const set<string> mt) { m_mimeTypes = mt; }
+    void setCallback(const Callback& cb) { m_cb = new Callback(cb); }
+    void setLimit(size_t l) { m_limit = l; }
+
+    void addChild(const Path& p,
+                  const Path& relPath) {
+        if (p.empty() || relPath.empty()) {
+            // yea, right
+            return;
+        }
+
+        // flat hierarchy is easy, only one list, all filehandles
+        if (m_flat) {
+            m_topList->append(new bplus::Path(p.externalUtf8()));
+            return;
+        }
+
+        // find list for relpath parent, creating any needed 
+        // lists along the way.
+        bplus::List* theList = NULL;
+        Path relParent = relPath.parent_path();
+        if (relParent.empty()) {
+            theList = m_topList;
+        } else {
+            // find anchor
+            Path tpathFull = p.parent_path();
+            for (tBase::iterator it = relParent.end(); it != relParent.begin(); --it) {
+                tpathFull = tpathFull.parent_path();
+            }
+            Path tpath;
+            for (tBase::iterator it = relParent.begin(); it != relParent.end(); ++it) {
+                tpath /= *it;
+                tpathFull /= *it;
+                if (m_listMap.find(tpath) == m_listMap.end()) {
+                    // no list for tpath, so it must be added to heirarchy
+                    bplus::Map* m = new bplus::Map;
+                    m->add("relativeName", new bplus::String(tpath.externalUtf8()));
+                    m->add("handle", new bplus::Path(tpathFull.externalUtf8()));
+                    bplus::List* kids = new bplus::List;
+                    m->add("children", kids);
+                    m_listMap[tpath] = kids;
+                    Path tparent = tpath.parent_path();
+                    if (tparent.empty()) {
+                        m_topList->append(m);
+                    } else {
+                        m_listMap[tparent]->append(m);
+                    }
+                }
+            }
+            theList = m_listMap[relParent];
+        }
+
+        // now add this node to list.  if it's a dir,
+        // also add new list to m_listMap
+        bplus::Map* m = new bplus::Map;
+        m->add("relativeName", new bplus::String(relPath.externalUtf8()));
+        m->add("handle", new bplus::Path(p.externalUtf8()));
+        if (isDirectory(p)) {
+            bplus::List* kids = new bplus::List;
+            m->add("children", kids);
+            m_listMap[relPath] = kids;
+        }
+        theList->append(m);
+    }
+
+    bplus::List* adoptKids() {
+        bplus::List* l = m_topList;
+        m_topList = new bplus::List;
+        return l;
+    }
+
+private:
+    bool m_flat;
+    set<string> m_mimeTypes;
+    size_t m_limit;
+    Callback* m_cb;
+    size_t m_visited;
+    map<Path, bplus::List*> m_listMap; // aliases into m_topList structure
+    bplus::List* m_topList; 
+};
+    
+
+// our service
+//
+class Directory : public Service
+{
+public:
+    BP_SERVICE(Directory);
+    
+    Directory() : Service() {
+    }
+    ~Directory() {
+    }
+
+    void list(const Transaction& tran, 
+              const bplus::Map& args);
+
+    void recursiveList(const Transaction& tran, 
+                       const bplus::Map& args);
+
+    void recursiveListWithStructure(const Transaction& tran, 
+                                    const bplus::Map& args);
+
+private:
+    void doList(const Transaction& tran, 
+                const bplus::Map& args,
+                bool recursive,
+                bool flat);
+};
+
+BP_SERVICE_DESC(Directory, "Directory", "2.0.6",
+                "Lets you list directory contents and invoke JavaScript ."
+                "callbacks for the contained items.")
+
+ADD_BP_METHOD(Directory, list,
+              "Returns a list in \"files\" of filehandles resulting "
+              "from a non-recursive traversal of the arguments.  "
+              "No directory structure information is returned. ")
+ADD_BP_METHOD_ARG(list, "files", List, true, 
+                  "Paths to traverse.")
+ADD_BP_METHOD_ARG(list, "followLinks", Boolean, false, 
+                  "If true, symbolic links will be followed. Default is true.")
+ADD_BP_METHOD_ARG(list, "mimetypes", List, false, 
+                  "Optional list of mimetype filters to apply (e.g."
+                  "[\"image/jpeg\"]")
+ADD_BP_METHOD_ARG(list, "limit", Integer, false, 
+                  "Maximum number of items to examine.  Default is 1000.")
+ADD_BP_METHOD_ARG(list, "callback", CallBack, false, 
+                  "Optional callback with will be invoked with each path.")
+
+ADD_BP_METHOD(Directory, recursiveList,
+              "Returns a list in \"files\" of filehandles resulting "
+              "from a recursive traversal of the arguments.  "
+              "No directory structure information is returned. ")
+ADD_BP_METHOD_ARG(recursiveList, "files", List, true, 
+                  "Paths to traverse.")
+ADD_BP_METHOD_ARG(recursiveList, "followLinks", Boolean, false, 
+                  "If true, symbolic links will be followed. Default is true.")
+ADD_BP_METHOD_ARG(recursiveList, "mimetypes", List, false, 
+                  "Optional list of mimetype filters to apply (e.g."
+                  "[\"image/jpeg\"]")
+ADD_BP_METHOD_ARG(recursiveList, "limit", Integer, false, 
+                  "Maximum number of items to examine.  Default is 1000.")
+ADD_BP_METHOD_ARG(recursiveList, "callback", CallBack, false, 
+                  "Optional callback with will be invoked with each path.")
+
+ADD_BP_METHOD(Directory, recursiveListWithStructure,
+              "Returns a nested list in \"files\" of objects for each of "
+              "the arguments.  An \"object\" contains the keys "
+              "\"relativeName\" (this node's name relative to the "
+              "specified directory), \"handle\" (a filehandle for "
+              "this node), and for directories \"children\" which "
+              "contains a list of objects for each of the directory's "
+              "children.  Recurse into directories.")
+ADD_BP_METHOD_ARG(recursiveListWithStructure, "files", List, true, 
+                  "Paths to traverse.")
+ADD_BP_METHOD_ARG(recursiveListWithStructure, "followLinks", Boolean, false, 
+                  "If true, symbolic links will be followed. Default is true.")
+ADD_BP_METHOD_ARG(recursiveListWithStructure, "mimetypes", List, false, 
+                  "Optional list of mimetype filters to apply (e.g."
+                  "[\"image/jpeg\"]")
+ADD_BP_METHOD_ARG(recursiveListWithStructure, "limit", Integer, false, 
+                  "Maximum number of items to examine.  Default is 1000.")
+ADD_BP_METHOD_ARG(recursiveListWithStructure, "callback", CallBack, false, 
+                  "Optional callback with will be invoked with each path.")
+
+END_BP_SERVICE_DESC
+
+
+void
+Directory::list(const Transaction& tran, 
+                const bplus::Map& args)
+{
+    doList(tran, args, false, true);
+}
+
+
+void
+Directory::recursiveList(const Transaction& tran, 
+                         const bplus::Map& args)
+{
+    doList(tran, args, true, true);
+}
+
+
+void
+Directory::recursiveListWithStructure(const Transaction& tran, 
+                                      const bplus::Map& args)
+{
+    doList(tran, args, true, false);
+}
+
+
+// here come's the heavy lifting...
+void
+Directory::doList(const Transaction& tran, 
+                  const bplus::Map& args,
+                  bool recursive,
+                  bool flat)
+{
+    try {
+        ListVisitor v(flat);
+
+        // dig out args
+
+        // files, required
+        const bplus::List* files = dynamic_cast<const bplus::List*>(args.value("files"));
+        if (!files) {
+            throw string("required files parameter missing");
+        }
+        
+        // followLinks, optional
+        bool followLinks = true;
+        (void) args.getBool("followLinks", followLinks);
+
+        // mimetypes, optional
+        set<string> mimeTypes;
+        const bplus::List* l = NULL;
+        (void) args.getList("mimetypes", l);
+        if (l) {
+            for (size_t i = 0; i < l->size(); i++) {
+                const bplus::String* s =
+                    dynamic_cast<const bplus::String*>(l->value(i));
+                if (s) {
+                    mimeTypes.insert(s->value());
+                }
+            }
+            v.setMimeTypes(mimeTypes);
+        }
+
+        // limit, optional
+        int limit = kDefaultLimit;
+        (void) args.getInteger("limit", limit);
+        v.setLimit(limit);
+
+        // callback, optional
+        const bplus::CallBack* cb =
+            dynamic_cast<const bplus::CallBack*>(args.value("callback"));
+        if (cb) {
+            v.setCallback(Callback(tran, *cb));
+        }
+
+        // do the visit, result in v.kids()
+        for (size_t i = 0; i < files->size(); i++) {
+            const bplus::Path* uri = dynamic_cast<const bplus::Path*>(files->value(i));
+            if (!uri) {
+                throw string("non-path argument found in 'files'");
+            }
+
+            Path path = pathFromURL((string)*uri);
+            if (recursive && isDirectory(path)) {
+                recursiveVisit(path, v, followLinks);
+            } else {
+                visit(path, v, followLinks);
+            }
+        }
+
+        // return massive success
+        bplus::Map results;
+        results.add("success", new bplus::Bool(true));
+        results.add("files", v.adoptKids());
+        tran.complete(results);
+
+    } catch (const string& msg) {
+        // one of our exceptions
+        log(BP_DEBUG, "Directory::list(), catch " + msg);
+        tran.error("directoryError", msg.c_str());
+
+    } catch (const tFileSystemError& e) {
+        // a boost::filesystem exception
+        string msg = string("Directory::list(), catch boost::filesystem")
+                     + " exception, path1: '" 
+                     + Path(e.path1()).utf8()
+                     +", path2: '" + Path(e.path2()).utf8()
+                     + "' (" + e.what() + ")";
+        log(BP_ERROR, "Directory: " + msg);
+        tran.error("directoryError", msg.c_str());
+    }
+}
+
---
+++ test_Directory/bp_directory/test/Directory_test.rb
@@ -0,0 +1,314 @@
+#BrowserPlus.FileAccess API Level Testing
+#bugs can be found at bugs.browserplus.org
+
+#!/usr/bin/env ruby
+ require 'logger'
+ 
+ require File.join( File.dirname(File.expand_path(__FILE__) ), 'util' )
+ 
+ 
+ 
+ if File.exist?('log.txt')
+	File.delete('log.txt')
+ end
+ 
+ 
+	
+ $log = Logger.new('log.txt')
+
+require File.join(File.dirname(File.dirname(File.expand_path(__FILE__))),
+                  'external/built/share/service_testing/bp_service_runner')
+require 'uri'
+require 'test/unit'
+require 'open-uri'
+
+#Directory
+#Lets you list directory contents and invoke JavaScript .callbacks for the contained items.
+class TestDirectory < Test::Unit::TestCase
+  #SETUP
+  def setup
+    curDir = File.dirname(__FILE__)
+    pathToService = File.join(curDir, "..", "src", "build", "Directory")
+    @s = BrowserPlus::Service.new(pathToService)
+    @path1 =  File.dirname(File.dirname(File.expand_path(__FILE__)))
+    @path_testdir = @path1+"/test_directory/"
+    @path_testdir_noP = @path1+"/test_directory"
+    @path_testdir1 = @path1+"/test_directory/test_directory_1/"
+    @test_directory = "file:///"+@path1+"/test_directory/"
+    @test_directory_1 = "file:///"+@path1+"/test_directory/test_directory_1"
+	
+	#@testid=1
+  end
+
+  #TEARDOWN
+  def teardown
+    @s.shutdown
+  end
+
+  def Add(add_one_to_me)
+    add_one_to_me = add_one_to_me + 1
+    return add_one_to_me
+  end
+
+  #BrowserPlus.Directory.list({params}, function{}())
+  #Returns a list in "files" of filehandles resulting from a non-recursive traversal of the arguments. No directory structure information is returned.
+  def test_list 
+	@x = "list"
+	#Directory/File does not exist, should return error <------------------------- BUG 212
+	#x = @test_directory_1+"/this"
+	#@list = Array.[]( x )
+	#got = @s.list( { 'files'=>@list  } ) { |callback| puts callback }
+	#puts got
+	
+#	@testid=@testid+1
+    # 3 text files.
+    @list = Array.[]( @test_directory_1 )
+    want = {"files"=> [@path_testdir1+"bar1.txt", @path_testdir1+"bar2.txt", @path_testdir1+"bar3.txt"], "success"=>true }
+    got = @s.list({ 'files' => @list })
+    assert_log( want, got, $log, @x, 1 )
+
+
+    #just one folder, no symbolic links
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt", @path_testdir+"sym_link", @path_testdir+"test_directory_1"],
+      "success"=>true}
+    got = @s.list({ 'files' => @list, "followLinks" => false })
+    assert_log( want, got, $log, @x, 1 )
+
+    #symbolic links
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt", @path1+"/sym_link", @path_testdir+"test_directory_1"],
+      "success"=>true}
+    got = @s.list({ 'files' => @list, "followLinks" => true })
+    assert_log( want, got, $log, @x, 1 )
+
+    
+    #mimetype => text/plain
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt"],
+      "success"=>true}
+    got = @s.list({ 'files' => @list, "followLinks" => false, "mimetypes" => ["text/plain"] })
+    assert_log( want, got, $log, @x, 1 )
+
+    #mimetype => image/jpeg ---- none present.
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [],
+      "success"=>true}
+    got = @s.list({ 'files' => @list, "followLinks" => false, "mimetypes" => ["image/jpeg"] })
+    assert_log( want, got, $log, @x, 1 )
+
+    #size = 2
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir+"foo1.txt", @path_testdir+"foo2.txt"],
+      "success"=>true}
+    got = @s.list({ 'files' => @list, "followLinks" => true, "limit"=>2 })
+    assert_log( want, got, $log, @x, 1 )
+
+
+    #callback
+    x = 1
+    @list = Array.[]( @test_directory )
+    got = @s.list({ 'files' => @list, "followLinks" => true, "limit"=>2 }, x = Add(x))
+    assert_equal( 2, x )
+
+  end
+
+
+  #BrowserPlus.Directory.recursiveList({params}, function{}())
+  #Returns a list in "files" of filehandles resulting from a recursive traversal of the arguments. No directory structure information is returned
+  def test_recursiveList
+	#Directory/File does not exist, should return error <--------------------------------- BUG 212
+	#x = @test_directory_1+"/this"
+	#@list = Array.[]( x )
+	#got = @s.recursiveList( { 'files'=>@list  } )
+	#puts got
+
+    # 3 text files.
+    @list = Array.[]( @test_directory_1 )
+    want = {"files"=> [@path_testdir+"test_directory_1", @path_testdir+"test_directory_1/bar1.txt", @path_testdir+"test_directory_1/bar2.txt", @path_testdir+"test_directory_1/bar3.txt"], "success"=>true }
+    got = @s.recursiveList({ 'files' => @list })
+    assert_log( want, got, $log, @x, 1 )
+
+
+    #just one folder, no symbolic links
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir_noP, @path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt", @path_testdir+"sym_link", @path_testdir+"test_directory_1", @path_testdir+"test_directory_1/bar1.txt", @path_testdir+"test_directory_1/bar2.txt", @path_testdir+"test_directory_1/bar3.txt"],
+      "success"=>true}
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => false })
+    assert_log( want, got, $log, @x, 1 )
+
+    #symbolic links
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir_noP, @path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt", @path1+"/sym_link", @path1+"/sym_link/sym1.txt",@path_testdir+"test_directory_1", @path_testdir+"test_directory_1/bar1.txt", @path_testdir+"test_directory_1/bar2.txt", @path_testdir+"test_directory_1/bar3.txt"],
+      "success"=>true}
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => true })
+    assert_log( want, got, $log, @x, 1 )
+
+    #mimetype => text/plain
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir+"foo1.txt", @path_testdir+"foo2.txt", @path_testdir+"foo3.txt", @path_testdir+"test_directory_1/bar1.txt", @path_testdir+"test_directory_1/bar2.txt", @path_testdir+"test_directory_1/bar3.txt"],
+      "success"=>true}
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => false, "mimetypes" => ["text/plain"] })
+    assert_log( want, got, $log, @x, 1 )
+
+
+    #mimetype => image/jpeg ---- none present.
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [],
+      "success"=>true}
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => false, "mimetypes" => ["image/jpeg"] })
+    assert_log( want, got, $log, @x, 1 )
+
+    #limit = 2
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [@path_testdir_noP, @path_testdir+"foo1.txt"],
+      "success"=>true}
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => true, "limit"=>2 })
+    assert_log( want, got, $log, @x, 1 )
+
+    #callback
+    x = 1
+    @list = Array.[]( @test_directory )
+    got = @s.recursiveList({ 'files' => @list, "followLinks" => true, "limit"=>2 }, x = Add(x))
+    assert_equal( 2, x )
+
+
+  end
+
+  #BrowserPlus.Directory.recursiveListWithStructure({params}, function{}())
+  #Returns a nested list in "files" of objects for each of the arguments. An "object" contains the keys "relativeName" (this node's name relative to the specified directory), 
+  #"handle" (a filehandle for this node), and for directories "children" which contains a list of objects for each of the directory's children. 
+  #Recurse into directories.
+  def test_recursiveListWithStructure
+	#Directory/File does not exist, should return error <--------------------------------- BUG 212
+	#x = @test_directory_1+"/this"
+	#@list = Array.[]( x )
+	#got = @s.recursiveListWithStructure( { 'files'=>@list  } )
+	#puts got
+
+	# 3 text files.
+    @list = Array.[]( @test_directory_1 )
+    want = {"files"=>
+        [{"handle"=>
+           @path_testdir+"test_directory_1",
+          "relativeName"=>"test_directory_1",
+          "children"=>
+           [{"handle"=>
+              @path_testdir+"test_directory_1/bar1.txt",
+             "relativeName"=>"test_directory_1/bar1.txt"},
+            {"handle"=>
+              @path_testdir+"test_directory_1/bar2.txt",
+             "relativeName"=>"test_directory_1/bar2.txt"},
+            {"handle"=>
+              @path_testdir+"test_directory_1/bar3.txt",
+             "relativeName"=>"test_directory_1/bar3.txt"}]}],
+       "success"=>true}
+    #want = {"files"=> [@path_testdir+"test_directory_1", @path_testdir+"test_directory_1/bar1.txt", @path_testdir+"test_directory_1/bar2.txt", @path_testdir+"test_directory_1/bar3.txt"], "success"=>true }
+    got = @s.recursiveListWithStructure({ 'files' => @list })
+    assert_log( want, got, $log, @x, 1 )
+
+    #just one folder, no symbolic links
+    @list = Array.[]( @test_directory )
+    want = {"files"=>
+        [{"handle"=>
+           @path_testdir_noP,
+          "relativeName"=>".",
+          "children"=>
+           [{"handle"=>
+              @path_testdir+"foo1.txt",
+             "relativeName"=>"./foo1.txt"},
+            {"handle"=>
+              @path_testdir+"foo2.txt",
+             "relativeName"=>"./foo2.txt"},
+            {"handle"=>
+              @path_testdir+"foo3.txt",
+             "relativeName"=>"./foo3.txt"},
+            {"handle"=>
+              @path_testdir+"sym_link",
+             "relativeName"=>"./sym_link",
+             "children"=>[]},
+            {"handle"=>
+              @path_testdir+"test_directory_1",
+             "relativeName"=>"./test_directory_1",
+             "children"=>
+              [{"handle"=>
+                 @path_testdir+"test_directory_1/bar1.txt",
+                "relativeName"=>"./test_directory_1/bar1.txt"},
+               {"handle"=>
+                 @path_testdir+"test_directory_1/bar2.txt",
+                "relativeName"=>"./test_directory_1/bar2.txt"},
+               {"handle"=>
+                 @path_testdir+"test_directory_1/bar3.txt",
+                "relativeName"=>"./test_directory_1/bar3.txt"}]}]}],
+       "success"=>true}
+    got = @s.recursiveListWithStructure({ 'files' => @list, "followLinks" => false })
+    assert_log( want, got, $log, @x, 1 )
+    
+    #mimetype => text/plain
+    @list = Array.[]( @test_directory )
+    want = {"files"=>
+        [{"handle"=>@path1+"/.",
+          "relativeName"=>".",
+          "children"=>
+           [{"handle"=>
+              @path_testdir+"foo1.txt",
+             "relativeName"=>"./foo1.txt"},
+            {"handle"=>
+              @path_testdir+"foo2.txt",
+             "relativeName"=>"./foo2.txt"},
+            {"handle"=>
+              @path_testdir+"foo3.txt",
+             "relativeName"=>"./foo3.txt"},
+            {"handle"=>
+              @path1+"/./test_directory_1",
+             "relativeName"=>"./test_directory_1",
+             "children"=>
+              [{"handle"=>
+                 @path_testdir+"test_directory_1/bar1.txt",
+                "relativeName"=>"./test_directory_1/bar1.txt"},
+               {"handle"=>
+                 @path_testdir+"test_directory_1/bar2.txt",
+                "relativeName"=>"./test_directory_1/bar2.txt"},
+               {"handle"=>
+                 @path_testdir+"test_directory_1/bar3.txt",
+                "relativeName"=>"./test_directory_1/bar3.txt"}]}]}],
+       "success"=>true}
+    got = @s.recursiveListWithStructure({ 'files' => @list, "followLinks" => false, "mimetypes" => ["text/plain"] })
+    assert_log( want, got, $log, @x, 1 )
+
+    #mimetype => image/jpeg ---- none present.
+    @list = Array.[]( @test_directory )
+    want = {"files"=> [],
+      "success"=>true}
+    got = @s.recursiveListWithStructure({ 'files' => @list, "followLinks" => false, "mimetypes" => ["image/jpeg"] })
+    assert_log( want, got, $log, @x, 1 )
+
+    #size = 2
+    @list = Array.[]( @test_directory )
+    want = {"files"=>
+        [{"handle"=>
+           @path_testdir_noP,
+          "relativeName"=>".",
+          "children"=>
+           [{"handle"=>
+              @path_testdir+"foo1.txt",
+             "relativeName"=>"./foo1.txt"}]}],
+       "success"=>true}
+    got = @s.recursiveListWithStructure({ 'files' => @list, "followLinks" => true, "limit"=>2 })
+    assert_log( want, got, $log, @x, 1 )
+
+    #function
+    x = 1
+    @list = Array.[]( @test_directory )
+    got = @s.recursiveListWithStructure({ 'files' => @list, "followLinks" => true, "limit"=>2 }, x = Add(x))
+    assert_equal( 2, x )
+
+
+  end
+
+
+
+
+
+  # XXX: test chunk and slice
+end
